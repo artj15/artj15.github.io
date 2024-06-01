c44e42393fa4cd86ed01e500c5b9ba0890a76979
myslivtsevar <artj15@gmail.com>
2024-06-01 12:06:10 +0300

    fix: files changed

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-function scrollDown() {
-    window.scrollTo(
-        {top: window.innerHeight, behavior: "smooth"}
-    )
-}
+let button = document.querySelector('.testJs')
+let h1 = document.querySelector('.testCss')
 
-let button = document.querySelector(".header_btn")
 button.addEventListener(
-    "click",
-    scrollDown
-)
-
-anime(
-    {
-        targets: ".header_left h1",
-        duration: 500,
-        easing: "linear",
-        opacity: 1
-    }
-)
-
-anime(
-    {
-        targets: ".header_left p",
-        duration: 500,
-        delay: 200,
-        easing: "linear",
-        opacity: 1
-    }
-)
-
-anime(
-    {
-        targets: ".header_btn",
-        duration: 300,
-        easing: "easeOutQuad",
-        translateY: [-window.innerHeight / 2, 0]
+    'click',
+    function () {
+        h1.innerHTML = 'Скрипт подключен.'
     }
 )
